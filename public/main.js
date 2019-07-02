4f39ff0f1c11f9e5476dfada8cad736f97d9a940
rhildred <rhildred@gmail.com>
2019-07-02 13:48:52 -0400

    have an actual map now

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
 import L from "leaflet";
 
+let mymap = null;
+
 function showPosition(oPosition){
-    alert("position lat: " + oPosition.coords.latitude + " long: " + oPosition.coords.longitude);
+    mymap = L.map('map-here').setView([oPosition.coords.latitude, oPosition.coords.longitude], 13);
+    // Use OpenStreetMap tiles and attribution
+    let osmTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    let attribution = '© OpenStreetMap contributors';
+
+    // Create the basemap and add it to the map
+    L.tileLayer(osmTiles, {
+        maxZoom: 18,
+        attribution: attribution
+    }).addTo(mymap);
 }
 
 window.addEventListener("load", () =>{
